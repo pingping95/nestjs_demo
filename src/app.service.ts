765a66961ecca92cbf1a5a17cbe6977002e720f0
pingping95 <xogns556@naver.com>
2022-04-26 03:37:12 +0900

    commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `DB HOST : ${process.env.DATABASE_HOST}`;
+    return `DB HOST : ${process.env.DATABASE_HOST}\nDB PASSWORD : ${process.env.DB_PASSWORD}`;
   }
 }
